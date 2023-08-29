ca7a24f1d54199d486cda7bb103c0260179a5804
Dex <maroderko8@gmail.com>
2023-08-29 18:03:16 +0300

    Initial commit

---
+++ src/main/java/com/example/newkafkastreamsproject/NewKafkaStreamsProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.newkafkastreamsproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewKafkaStreamsProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewKafkaStreamsProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/newkafkastreamsproject/NewKafkaStreamsProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.newkafkastreamsproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewKafkaStreamsProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
